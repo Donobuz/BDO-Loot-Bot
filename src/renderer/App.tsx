0be130c5c8790a76c69b3654221592f86cebe7c9
Groveybear <donobuz@gmail.com>
2025-07-11 03:21:20 -0400

    switch from react native to electron

---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 50 }}>
+      <h1>Hello from Electron + React + TypeScript!</h1>
+    </div>
+  );
+}
